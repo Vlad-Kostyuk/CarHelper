79ed941866eb2788d984dc8ec95ae430b6d54382
Vlad <vlad.kostyuk24@gmail.com>
2018-12-11 15:12:39 +0200

    Add home Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/screens/note_list.dart';
-import 'package:flutter_app/screens/note_detail.dart';
-
+import 'dart:async';
+import 'package:flutter_app/models/note.dart';
+import 'package:flutter_app/utils/database_date_teahouse.dart';
+import 'package:flutter_app/screens/note_list_date_teahouse.dart';
+import 'package:flutter_app/screens/note_detail_date_teahouse.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 void main() {
-	runApp(MyApp());
+  runApp(MaterialApp(
+
+    home: FirstScreen(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -12,12 +19,42 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return MaterialApp(
-      title: 'Car Helper',
 	    debugShowCheckedModeBanner: false,
 	    theme: ThemeData(
 		    primarySwatch: Colors.green
 	    ),
 	    home: NoteList(),
+
     );
   }
 }
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+
+    return Scaffold(
+
+      appBar: AppBar(
+        title: Text('Car Helper'),
+
+      ),
+      body: Center(
+
+        child: RaisedButton(
+          child: Text('Перейти ло техоглядів'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => NoteList()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+
+
